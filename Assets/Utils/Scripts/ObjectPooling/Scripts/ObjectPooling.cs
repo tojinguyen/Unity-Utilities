f76fa5a3a997fd72c41612ddbdd39efdbcc8aab0
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-04-22 21:28:24 +0700

    feat(pool): update pool object, component

--- Assets/Utils/Scripts/ObjectPooling/Scripts/ObjectPooling.cs
+++ Assets/Utils/Scripts/ObjectPooling/Scripts/ObjectPooling.cs
@@ -3,116 +3,240 @@ using UnityEngine;
 
 public static class ObjectPooling
 {
-    private static Dictionary<GameObject, Queue<GameObject>> _pools = new();
+    private static readonly Dictionary<GameObject, Queue<Component>> Pools = new();
+    private static readonly Dictionary<Component, GameObject> InstanceToPrefab = new();
 
-    /// <summary>
-    /// Preloads a specified number of objects into the pool for a given prefab.
-    /// </summary>
-    /// <param name="prefab">The prefab to preload.</param>
-    /// <param name="count">The number of objects to preload.</param>
-    public static void Prewarm(GameObject prefab, int count)
+    // ======= COMPONENT VERSIONS =======
+
+    public static void Prewarm<T>(T prefab, int count) where T : Component
     {
-        if (!_pools.TryGetValue(prefab, out var pool))
+        var prefabGo = prefab.gameObject;
+
+        if (!Pools.TryGetValue(prefabGo, out var pool))
         {
-            pool = new Queue<GameObject>();
-            _pools[prefab] = pool;
+            pool = new Queue<Component>();
+            Pools[prefabGo] = pool;
         }
 
         for (var i = 0; i < count; i++)
         {
-            var obj = Object.Instantiate(prefab);
-            obj.SetActive(false);
-            pool.Enqueue(obj);
+            var instance = Object.Instantiate(prefab);
+            instance.gameObject.SetActive(false);
+            pool.Enqueue(instance);
+            InstanceToPrefab[instance] = prefabGo;
         }
     }
 
-    /// <summary>
-    /// Retrieves an object from the pool or creates a new one if none are available.
-    /// </summary>
-    /// <param name="prefab">The prefab to use as a template.</param>
-    /// <returns>A GameObject from the pool.</returns>
+    public static T GetObject<T>(T prefab) where T : Component
+    {
+        var prefabGo = prefab.gameObject;
+
+        if (!Pools.TryGetValue(prefabGo, out var pool))
+        {
+            pool = new Queue<Component>();
+            Pools[prefabGo] = pool;
+        }
+
+        var instance = pool.Count > 0
+            ? (T)pool.Dequeue()
+            : Object.Instantiate(prefab);
+
+        instance.gameObject.SetActive(true);
+        InstanceToPrefab[instance] = prefabGo;
+
+        return instance;
+    }
+
+
+    public static T GetObject<T>(T prefab, Transform parent) where T : Component
+    {
+        var instance = GetObject(prefab);
+        instance.transform.SetParent(parent, false);
+        return instance;
+    }
+
+    public static T GetObject<T>(T prefab, Vector3 position) where T : Component
+    {
+        var instance = GetObject(prefab);
+        instance.transform.position = position;
+        return instance;
+    }
+
+    public static T GetObject<T>(T prefab, Quaternion rotation) where T : Component
+    {
+        var instance = GetObject(prefab);
+        instance.transform.rotation = rotation;
+        return instance;
+    }
+
+    public static T GetObject<T>(T prefab, Vector3 position, Quaternion rotation) where T : Component
+    {
+        var instance = GetObject(prefab);
+        instance.transform.position = position;
+        instance.transform.rotation = rotation;
+        return instance;
+    }
+
+    public static T GetObject<T>(T prefab, Transform parent, Vector3 localPosition) where T : Component
+    {
+        var instance = GetObject(prefab);
+        instance.transform.SetParent(parent, false);
+        instance.transform.localPosition = localPosition;
+        return instance;
+    }
+
+    public static T GetObject<T>(T prefab, Transform parent, Vector3 localPosition, Quaternion localRotation)
+        where T : Component
+    {
+        var instance = GetObject(prefab);
+        instance.transform.SetParent(parent, false);
+        instance.transform.localPosition = localPosition;
+        instance.transform.localRotation = localRotation;
+        return instance;
+    }
+
+    // ======= GAME OBJECT VERSIONS =======
+
+    public static void Prewarm(GameObject prefab, int count)
+    {
+        var component = prefab.GetComponent<Component>();
+        if (component == null)
+        {
+            ConsoleLogger.LogError("[ObjectPooling] GameObject has no Component to pool.");
+            return;
+        }
+
+        Prewarm(component, count);
+    }
+
     public static GameObject GetObject(GameObject prefab)
     {
-        if (!_pools.TryGetValue(prefab, out var pool))
+        var component = prefab.GetComponent<Component>();
+        if (component == null)
         {
-            pool = new Queue<GameObject>();
-            _pools[prefab] = pool;
+            ConsoleLogger.LogError("[ObjectPooling] GameObject has no Component to pool.");
+            return null;
         }
 
-        if (pool.Count > 0)
-        {
-            var obj = pool.Dequeue();
-            obj.SetActive(true);
-            return obj;
-        }
-
-        // Create a new object if the pool is empty
-        var newObj = Object.Instantiate(prefab);
-        return newObj;
+        var instance = GetObject(component);
+        return instance.gameObject;
     }
 
-    /// <summary>
-    /// Returns an object to the pool for reuse.
-    /// </summary>
-    /// <param name="prefab">The prefab associated with this object.</param>
-    /// <param name="obj">The GameObject to return to the pool.</param>
-    public static void ReturnObject(GameObject prefab, GameObject obj)
+    public static GameObject GetObject(GameObject prefab, Transform parent)
     {
-        if (!_pools.TryGetValue(prefab, out var pool))
+        var instance = GetObject(prefab);
+        instance.transform.SetParent(parent, false);
+        return instance;
+    }
+
+    public static GameObject GetObject(GameObject prefab, Vector3 position)
+    {
+        var instance = GetObject(prefab);
+        instance.transform.position = position;
+        return instance;
+    }
+
+    public static GameObject GetObject(GameObject prefab, Quaternion rotation)
+    {
+        var instance = GetObject(prefab);
+        instance.transform.rotation = rotation;
+        return instance;
+    }
+
+    public static GameObject GetObject(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        var instance = GetObject(prefab);
+        instance.transform.position = position;
+        instance.transform.rotation = rotation;
+        return instance;
+    }
+
+    public static GameObject GetObject(GameObject prefab, Transform parent, Vector3 localPosition)
+    {
+        var instance = GetObject(prefab);
+        instance.transform.SetParent(parent, false);
+        instance.transform.localPosition = localPosition;
+        return instance;
+    }
+
+    public static GameObject GetObject(GameObject prefab, Transform parent, Vector3 localPosition,
+        Quaternion localRotation)
+    {
+        var instance = GetObject(prefab);
+        instance.transform.SetParent(parent, false);
+        instance.transform.localPosition = localPosition;
+        instance.transform.localRotation = localRotation;
+        return instance;
+    }
+
+    // ======= RETURN & CLEAR =======
+
+    public static void ReturnObject(Component instance)
+    {
+        if (!InstanceToPrefab.TryGetValue(instance, out var prefabGo))
         {
-            pool = new Queue<GameObject>();
-            _pools[prefab] = pool;
+            ConsoleLogger.LogWarning($"[ObjectPooling] Tried to return unknown object: {instance.name}");
+            Object.Destroy(instance.gameObject);
+            return;
         }
 
-        obj.SetActive(false);
-        pool.Enqueue(obj);
+        instance.gameObject.SetActive(false);
+        Pools[prefabGo].Enqueue(instance);
     }
-    
-    /// <summary>
-    /// Clears all objects from the pool for the specified prefab.
-    /// All pooled objects will be destroyed.
-    /// </summary>
-    /// <param name="prefab">The prefab whose pool should be cleared.</param>
-    public static void ClearPool(GameObject prefab)
+
+    public static void ReturnObject(GameObject obj)
     {
-        if (!_pools.TryGetValue(prefab, out var pool))
+        var component = obj.GetComponent<Component>();
+        if (component == null)
+        {
+            ConsoleLogger.LogWarning("[ObjectPooling] Tried to return a GameObject with no Component.");
+            Object.Destroy(obj);
+            return;
+        }
+
+        ReturnObject(component);
+    }
+
+    public static void ClearPool<T>(T prefab) where T : Component
+    {
+        var prefabGo = prefab.gameObject;
+
+        if (!Pools.TryGetValue(prefabGo, out var pool))
             return;
 
         while (pool.Count > 0)
         {
-            var obj = pool.Dequeue();
-            Object.Destroy(obj);
+            var instance = pool.Dequeue();
+            InstanceToPrefab.Remove(instance);
+            Object.Destroy(instance.gameObject);
         }
 
-        _pools.Remove(prefab);
+        Pools.Remove(prefabGo);
     }
 
-
-    /// <summary>
-    /// Clears all pools and destroys all objects.
-    /// </summary>
     public static void ClearAllPools()
     {
-        foreach (var pool in _pools.Values)
+        foreach (var pool in Pools.Values)
         {
             while (pool.Count > 0)
             {
-                var obj = pool.Dequeue();
-                Object.Destroy(obj);
+                var instance = pool.Dequeue();
+                InstanceToPrefab.Remove(instance);
+                Object.Destroy(instance.gameObject);
             }
         }
 
-        _pools.Clear();
-    }
-    
-    /// <summary>
-    /// Gets the current size of the pool for a specific prefab.
-    /// </summary>
-    /// <param name="prefab">The prefab whose pool size is queried.</param>
-    /// <returns>The number of objects currently in the pool.</returns>
-    public static int GetPoolSize(GameObject prefab)
-    {
-        return _pools.TryGetValue(prefab, out var pool) ? pool.Count : 0;
+        Pools.Clear();
+        InstanceToPrefab.Clear();
     }
 
+    public static int GetPoolSize<T>(T prefab) where T : Component
+    {
+        return Pools.TryGetValue(prefab.gameObject, out var pool) ? pool.Count : 0;
+    }
+
+    public static int GetPoolSize(GameObject prefab)
+    {
+        return Pools.TryGetValue(prefab, out var pool) ? pool.Count : 0;
+    }
 }
