fc7055710a667cf80630a076db6b3ed191bee53e
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-04-22 21:29:35 +0700

    feat(tool-scene): add scripting define

--- Scripts/Editor/SceneToolbarUtilities.cs
+++ Scripts/Editor/SceneToolbarUtilities.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -139,3 +140,4 @@ public static class SceneToolbarUtilities
         }
     }
 }
+#endif
