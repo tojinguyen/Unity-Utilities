e27dd567871deccb1ad23e0c7a107547fabe22c3
toji-dev <toainguyenprogramminggame@gmail.com>
2025-09-13 22:30:46 +0700

    feat: implement tab

---
+++ Assets/Utils/Scripts/UIManager/UINavigator/Tab/Tab.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utils.Scripts.UIManager.UINavigator.Tab
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class Tab : EView
+    {
+        public Toggle ToggleThisTab;
+    }
+}
