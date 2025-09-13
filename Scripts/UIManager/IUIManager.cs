3f488f0984a79b2a408f5484af165429a3e1c6c2
toji-dev <toainguyenprogramminggame@gmail.com>
2025-09-13 14:29:08 +0700

    feat: remove error files

--- Scripts/UIManager/IUIManager.cs
+++
@@ -1,8 +0,0 @@
-using Cysharp.Threading.Tasks;
-
-public interface IUIManager
-{
-    public UniTask<T> ShowUI<T>(string address) where T : UIBase;
-    public UniTask Pop();
-    public UIBase Peek();
-}
